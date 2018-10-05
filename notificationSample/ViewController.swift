e2ea7b1a9af33f9947b386b836c720e6958d0084
Katsuhiko Hayashi <katsuhiko.hayashi.847@gmail.com>
2018-10-05 12:52:05 +0900

    Initial Commit

---
+++ notificationSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  notificationSample
+//
+//  Created by 林克彦 on 2018/10/05.
+//  Copyright © 2018年 Katsuhiko Hayashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
